8ea70b27ec61636f14804e246e72d07f72b97857
m3ales <mattfdavies12@gmail.com>
2021-07-22 22:17:28 +0200

    Define requirements as test

---
+++ test/Application.Tests/Acceptance.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TaskManager.Api.Application.Tests
+{
+    /// <summary>
+    /// The requirements
+    /// </summary>
+    public class Acceptance
+    {
+        [Fact]
+        public void Should_Modify_Progress_Of_Task()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Show_Progress_Of_Task()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Assign_Tasks_To_Team_Members()
+        {
+            //Arrange
+            
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Add_Tasks()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Modify_Tasks()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Remove_Tasks()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Add_TeamMembers()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Modify_TeamMembers()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void Should_Remove_TeamMembers()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
