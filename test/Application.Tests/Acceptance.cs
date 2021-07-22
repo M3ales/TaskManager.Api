aa184c9678747a52cb04d1871fbb930ad5cd6d18
m3ales <mattfdavies12@gmail.com>
2021-07-22 23:41:30 +0200

    Wrote first red test
    Will now work on making it green, then do a refactor pass.
    
    Once I've got it Blue I'll look at maybe setting up some CI via github actions.

--- test/Application.Tests/Acceptance.cs
+++ test/Application.Tests/Acceptance.cs
@@ -1,12 +1,50 @@
-﻿using System;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace TaskManager.Api.Application.Tests
 {
+
+    // CQRS
+    public interface IApplicationDbContext
+    {
+        ICollection<TeamMember> TeamMembers { get; }
+        ICollection<TaskItem> TaskItems { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+
+    public class AssignTaskToTeamMemberCommand : IRequest
+    {
+        public int TaskId { get; set; }
+        public int TeamMemberId { get; set; }
+    }
+
+    public class AssignTaskToTeamMemberCommandHandler : IRequestHandler<AssignTaskToTeamMemberCommand, Unit>
+    {
+        public Task<Unit> Handle(AssignTaskToTeamMemberCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public TeamMember AssignedTo { get; set; }
+    }
+
+    public class TeamMember
+    {
+        public int Id { get; set; }
+    }
+
     /// <summary>
     /// The requirements
     /// </summary>
@@ -31,13 +69,40 @@ namespace TaskManager.Api.Application.Tests
             //Assert
         }
         [Fact]
-        public void Should_Assign_Tasks_To_Team_Members()
+        public async Task Should_Assign_Tasks_To_Team_Members()
         {
             //Arrange
-            
-            //Act
+            // TODO: replace hardcoded values with an autofixture to generate random values
+            var teamMembers = new List<TeamMember>() { new TeamMember() { Id = 0 } }; 
+            var tasks = new List<TaskItem>() { new TaskItem() { Id = 0 } };
+            var request = new AssignTaskToTeamMemberCommand()
+            {
+                TaskId = 0,
+                TeamMemberId = 0
+            };
+            var cancellationSource = new CancellationTokenSource();
 
+            var _applicationDbContext = new Mock<IApplicationDbContext>();
+            _applicationDbContext
+                .SetupGet(context => context.TaskItems)
+                .Returns(tasks)
+                .Verifiable("Must get the task");
+            _applicationDbContext
+                .SetupGet(context => context.TeamMembers)
+                .Returns(teamMembers)
+                .Verifiable("Must get the team member");
+            _applicationDbContext.Setup(x => x.SaveChangesAsync(
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(0))
+                .Verifiable("Must save the resulting change");
+
+            var sut = new AssignTaskToTeamMemberCommandHandler();
+            //Act
+            var result = await sut.Handle(request, cancellationSource.Token);
             //Assert
+            tasks.Count.Should().Be(1, "because you shouldn't be adding any new tasks");
+            teamMembers.Count.Should().Be(1, "because you shouldn't be adding any new team members");
+            tasks[0].AssignedTo.Should().Be(teamMembers[0], "because you should assign the team member to the task");
         }
         [Fact]
         public void Should_Add_Tasks()
