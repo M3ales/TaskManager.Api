799aa59021aad26c6f4e13fde6f9a015de51e239
m3ales <mattfdavies12@gmail.com>
2021-07-24 18:18:29 +0200

    Add GetAllWorkItems

---
+++ src/Application/WorkItems/Queries/GetAllWorkItems/GetAllWorkItemsQuery.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskManager.Api.Application.Common.Exceptions;
+using TaskManager.Api.Application.Common.Interfaces;
+using TaskManager.Api.Application.WorkItems.Queries.Common;
+using TaskManager.Api.Application.WorkItems.Queries.GetWorkItem;
+using TaskManager.Api.Domain.Entities;
+
+namespace TaskManager.Api.Application.WorkItems.Queries.GetAllWorkItems
+{
+    public class GetAllWorkItemsQuery : IRequest<List<ShallowWorkItemDto>>
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetAllWorkItemsQueryHandler : IRequestHandler<GetAllWorkItemsQuery, List<ShallowWorkItemDto>>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+        private readonly IMapper _mapper;
+        public GetAllWorkItemsQueryHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            _applicationDbContext = applicationDbContext;
+            _mapper = mapper;
+
+        }
+        public Task<List<ShallowWorkItemDto>> Handle(GetAllWorkItemsQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(
+                _applicationDbContext
+                    .WorkItems
+                    .AsQueryable()
+                    .ProjectTo<ShallowWorkItemDto>(_mapper.ConfigurationProvider)
+                    .ToList());
+        }
+    }
+}
---
+++ test/Application.Tests/WorkItems/Queries/GetAllWorkItems/GetAllWorkItemsQueryTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskManager.Api.Application.Common.Interfaces;
+using TaskManager.Api.Application.WorkItems.Queries.Common;
+using TaskManager.Api.Application.WorkItems.Queries.GetAllWorkItems;
+using TaskManager.Api.Domain.Entities;
+using Xunit;
+
+namespace TaskManager.Api.Application.Tests.WorkItems.Queries.GetAllWorkItems
+{
+    public class GetAllWorkItemsQueryTests
+    {
+        [AutoMoqData]
+        [InlineAutoMoqData]
+        [InlineAutoMoqData]
+        [InlineAutoMoqData]
+        [InlineAutoMoqData]
+        [InlineAutoMoqData]
+        [Theory(DisplayName = "Should all work items")]
+        public async Task Should_Get_All_WorkItems(
+               CancellationTokenSource cancellationSource,
+               [Frozen] Mock<IApplicationDbContext> applicationDbContext,
+               IMapper mapper,
+               List<WorkItem> workItems,
+               GetAllWorkItemsQueryHandler sut,
+               GetAllWorkItemsQuery request
+             )
+        {
+            //Arrange
+            applicationDbContext.Setup(context => context.WorkItems).Returns(workItems);
+
+            //Act
+            var result = await sut.Handle(request, cancellationSource.Token);
+
+            //Assert
+            result.Should().BeEquivalentTo(mapper.ProjectTo<ShallowWorkItemDto>(workItems.AsQueryable()).ToList(), "because it should return the equivalent of the requested work item as a DTO");
+        }
+    }
+}
