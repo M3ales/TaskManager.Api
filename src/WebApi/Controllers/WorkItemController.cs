ee52e9c3ec2b0811baba4d23e4a23589372632b8
m3ales <mattfdavies12@gmail.com>
2021-08-01 14:55:59 +0200

    Add CRUD endpoints for WorkItem

--- src/WebApi/Controllers/WorkItemController.cs
+++ src/WebApi/Controllers/WorkItemController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TaskManager.Api.Application.WorkItems.Commands.CreateWorkItem;
+using TaskManager.Api.Application.WorkItems.Commands.DeleteWorkItem;
+using TaskManager.Api.Application.WorkItems.Commands.UpdateWorkItem;
 using TaskManager.Api.Application.WorkItems.Queries.Common;
 using TaskManager.Api.Application.WorkItems.Queries.GetAllWorkItems;
 using TaskManager.Api.Application.WorkItems.Queries.GetWorkItem;
@@ -16,5 +20,18 @@ namespace WebApi.Controllers
         public async Task<ActionResult<List<ShallowWorkItemDto>>> Get() => await Mediator.Send(new GetAllWorkItemsQuery());
         [HttpGet]
         public async Task<ActionResult<ShallowWorkItemDto>> Get([FromQuery] GetWorkItemQuery request) => await Mediator.Send(request);
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreateWorkItemCommand request) => await Mediator.Send(request);
+
+        [HttpPut]
+        public async Task<ActionResult<ShallowWorkItemDto>> Update([FromBody] UpdateWorkItemCommand request) => await Mediator.Send(request);
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete([FromBody] DeleteWorkItemCommand request)
+        {
+            await Mediator.Send(request);
+            return NoContent();
+        }
     }
 }
